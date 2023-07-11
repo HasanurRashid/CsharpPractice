bd414d07aa31407771360a6cedadf7ee3c6bcd0a
Hasanur Rashid <hasan6221@gmail.com>
2023-07-12 01:03:07 +0600

    Static And Instance Class Added

---
+++ StaticAnd_InstaceClasses/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAnd_InstaceClasses
+{
+    public class Circle
+    {
+        public static float _pi;
+        int _radious;
+        static Circle()
+        {
+            _pi = 3.14f;
+        }
+        public Circle(int radious)
+        {
+            this._radious = radious;
+        }
+
+        public float CircleArea()
+        {
+            return _pi * _radious * _radious;
+        }
+
+    }
+}
---
+++ StaticAnd_InstaceClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StaticAnd_InstaceClasses;
+
+Circle circle = new Circle(5);
+
+float circleArea =  circle.CircleArea();
+Console.WriteLine(circleArea);
+
+Console.WriteLine($"Static Constructor Called : {Circle._pi}");
+
+
+
