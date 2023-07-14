99cb3d0776f22db54261dcf24b4ca050120aa1d1
Hasanur Rashid <hasan6221@gmail.com>
2023-07-14 17:16:13 +0600

    Method Overloading Added

---
+++ MethodOverloading/Method.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public static class Method
+    {
+        public static void Add(int fn, int sn)
+        {
+            Console.WriteLine(fn + sn);
+
+        }
+        public static void Add(int fn,int sn,out int sum,out int multi)
+        {
+            sum = fn + sn;
+            multi = fn*sn;  
+           
+        }
+
+        public static void Add(int fn, int sn, out int multi)
+        {
+         
+            multi = fn * sn;
+
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using MethodOverloading;
+
+int result = 0;
+int multi = 0;
+Method.Add(5, 6, out result,out multi);
+
+Console.WriteLine(result+","+multi);
