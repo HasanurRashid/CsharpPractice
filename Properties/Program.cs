22ed0fa4380056e395ddfe3f1340e095526bad75
Hasanur Rashid <hasan6221@gmail.com>
2023-07-21 23:55:17 +0600

    Properties Added

---
+++ Properties/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Properties;
+
+Student student = new Student();
+
+Console.WriteLine(student.Id = 100);
+Console.WriteLine(student.Id=-100);
+
---
+++ Properties/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Student
+    {
+        private  int _id;
+        private string Name { get; set; }
+
+        private int Marks { get;}
+
+        public int Id
+        {
+            get
+            {
+                return _id > 0 ? _id : 0;
+
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Invalid number. Try again!");
+                }
+                _id = value;
+            }
+        }
+       
+    }
+}
