f6d0bbe55d4bb221381d8f7adc015fabfec33dff
Hasanur Rashid <hasan6221@gmail.com>
2023-07-29 18:25:37 +0600

    Generic Added

---
+++ GenericExample/Coordinate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericExample
+{
+    public class Coordinate<T,Q> where T : class, new() where Q : struct
+    {
+        public T Age { get; set; }
+        public Q Name { get; set; }
+
+        public void Add<K>(K value)
+        {
+            T t;
+            Q q;
+            K k;
+        }
+
+
+    }
+}
---
+++ GenericExample/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GenericExample;
+
+Coordinate<int,string> coordinate = new Coordinate<int,string>();
+
+coordinate.Age= 1;
+coordinate.Name = "Hasan";
+
+Console.WriteLine(coordinate.Name);
