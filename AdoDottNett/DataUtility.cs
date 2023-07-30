b6e0a172d6d496d30251f6f2f6368aa4d7def305
Hasanur Rashid <hasan6221@gmail.com>
2023-07-30 18:41:04 +0600

    More Ado dotnet

--- AdoDottNett/DataUtility.cs
+++ AdoDottNett/DataUtility.cs
@@ -9,16 +9,55 @@ namespace AdoDottNett
 {
     public class DataUtility
     {
-        public void InsertData()
+        private SqlConnection _connection;
+        public DataUtility(string connectionString)
         {
-            string connectionString = "Server=MAAQ-HP;Database=CsharpDB;User Id=sa;Password=M@@q_786;TrustServerCertificate=true";
-            SqlConnection connection = new SqlConnection(connectionString);
-            string sql = @"Insert Into Student(ID,[Name],Class) values (2,'Mamun',9)";
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = connection;
-            cmd.CommandText = sql;
-            if (connection.State != System.Data.ConnectionState.Open) connection.Open();
+            _connection = new SqlConnection(connectionString);
+        }
+       
+        public void ExecuteSql(string sql, Dictionary<string, object> values)
+        {
+            using SqlCommand cmd = CreateCommand(sql, values);
             cmd.ExecuteNonQuery();
+        }
+
+        public IList<Dictionary<string, object>> DataRead(string sql, Dictionary<string, object> values)
+        {
+            using SqlCommand cmd = CreateCommand(sql, values);
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Dictionary<string, object>> data = new List<Dictionary<string, object>>();
+            while (reader.Read())
+            {
+                Dictionary<string, object> row = new Dictionary<string, object>();
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row.Add(reader.GetName(i), reader[i]);
+                }
+                data.Add(row);
+            }
+
+            return data;
+        }
+
+
+        private SqlCommand CreateCommand(string sql,Dictionary<string,object> values)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+           if(values !=null && values.Count > 0)
+            {
+                foreach (var item in values)
+                {
+                    cmd.Parameters.Add(new SqlParameter(item.Key, item.Value));
+                }
+            }
+
+            cmd.Connection = _connection;
+            cmd.CommandText = sql;
+            if (_connection.State != System.Data.ConnectionState.Open) _connection.Open();
+            return cmd;
         }
     }
 }
--- AdoDottNett/Program.cs
+++ AdoDottNett/Program.cs
@@ -2,5 +2,27 @@
 
 using AdoDottNett;
 
-DataUtility dataUtility = new DataUtility();
-dataUtility.InsertData();
+string connectionString = "Server=MAAQ-HP;Database=CsharpDB;User Id=sa;Password=M@@q_786;TrustServerCertificate=true";
+DataUtility dataUtility = new DataUtility(connectionString);
+
+//string sql = @"update Student set Name = 'Ahsan_Update' where name ='Ahsan'";
+//string sql = @"Delete Student  where name ='Ahsan_Update'";
+string sql = @"Select * from Student where name =@name";
+//dataUtility.ExecuteSql(sql);
+var name = Console.ReadLine();
+
+var parameters = new Dictionary<string, object>
+{
+    { "@name",name}
+};
+
+IList< Dictionary<string,object> > data = dataUtility.DataRead(sql, parameters);
+
+foreach(var item in data)
+{
+    foreach(var parameter in item)
+    {
+
+    }
+    Console.WriteLine($"ID: {item["ID"]}, Name: {item["Name"]},Class: {item["Class"]}");
+}
