15565bf1b19f86cc5343f33fe24ea9fa9cefe483
Hasanur Rashid <hasan6221@gmail.com>
2023-07-30 21:10:09 +0600

    Entity Framework Added

---
+++ EntityFrameWorkExample/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EntityFrameWorkExample;
+using System.Security.Cryptography.X509Certificates;
+
+StudentDbContext context = new StudentDbContext();
+
+context.Students.Add(new Student { Name = "Hasan", Class = "MA", AdmissionDate = DateTime.Now, IsRegular = true});
+context.Students.Add(new Student { Name = "C##", Class = "MA", AdmissionDate = DateTime.Now, IsRegular = true });
+context.SaveChanges();
+
+Student student = context.Students.Where(x=> x.Id==2).FirstOrDefault();
+
+Console.WriteLine($"Name: {student.Name} Class: {student.Class}");
+
+student.Name = "Angular";
+context.SaveChanges();
+
+context.Students.Remove(student);
+context.SaveChanges();
---
+++ EntityFrameWorkExample/StudentDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkExample
+{
+    public class StudentDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public StudentDbContext()
+        {
+            _connectionString = "Server=MAAQ-HP;Database=CsharpDB;User Id=sa;Password=M@@q_786;TrustServerCertificate=true";
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+           
+        }
+      public  DbSet<Student> Students { get; set; }    
+    }
+}
