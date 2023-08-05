014f655f91c2edf23ecd1425a3a3a880a5991e87
Hasanur Rashid <hasan6221@gmail.com>
2023-08-06 02:04:06 +0600

    More on delegates

---
+++ MultiCastDelegate/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MultiCastDelegate;
+using static MultiCastDelegate.SampleOne;
+using static MultiCastDelegate.SampleThree;
+using static MultiCastDelegate.SampleTwo;
+
+Sample_1 del1, del2, del3, del4;
+
+del1 = new Sample_1(Method_1);
+del2 = new Sample_1(Method_2);
+del3 = new Sample_1(Method_3);
+
+del4 = del1 + del2 + del3 - del1;
+
+del4();
+
+Sample_2 sample_2 = new Sample_2(Method1);
+sample_2 += new Sample_2(Method2);
+sample_2 += new Sample_2(Method3);
+sample_2 -= new Sample_2(Method1);
+
+sample_2();
+
+Sample_3 Sam1, sam2,sam3;
+
+Sam1 = new Sample_3(MethodReturn_1);
+sam2 = new Sample_3(MethodReturn_2);
+
+sam3 = Sam1 + sam2;
+
+int delReturnedValue = sam3();
+
+Console.WriteLine($"{delReturnedValue}");
+
---
+++ MultiCastDelegate/SampleOne.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCastDelegate
+{
+    public class SampleOne
+    {
+        public delegate void Sample_1();
+
+        public static void Method_1()
+        {
+            Console.WriteLine("Method_1 Invocked");
+        }
+        public static void Method_2()
+        {
+            Console.WriteLine("Method_2 Invocked");
+        }
+        public static void Method_3()
+        {
+            Console.WriteLine("Method_3 Invocked");
+        }
+
+       
+    }
+
+    public class SampleTwo
+    {
+        public delegate void Sample_2();
+
+        public static void Method1()
+        {
+            Console.WriteLine("Method_1 Invocked");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Method_2 Invocked");
+        }
+        public static void Method3()
+        {
+            Console.WriteLine("Method_3 Invocked");
+        }
+    }
+
+    public class SampleThree
+    {
+        public delegate int  Sample_3();
+        public static int MethodReturn_1()
+        {
+            return 1;
+        }
+        public static int MethodReturn_2()
+        {
+            return 2;
+        }
+    }
+}
