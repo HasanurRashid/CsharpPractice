c16119083e2aec59754fd587af21203c4884bb04
Hasanur Rashid <hasan6221@gmail.com>
2023-09-28 19:44:46 +0600

    Visual Studio code snippet added

---
+++ VisualStudioCodeSnippet/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+for (int i = 0; i < 10; i++) // Write just for then press tab twice
+{
+
+}
+
+for (int i = 0; i < 20; i++)
+{
+
+}
+
+for (int j = 0; j < 10; j++)
+{
+
+}
+for (int k = 0; k < 20; k++)
+{
+
+}
+do
+{
+
+} while (true);
+
+checked
+{
+
+}
+
